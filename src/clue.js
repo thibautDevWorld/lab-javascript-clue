dfe06c77907c10cf158a7fed0f2fe3d27c6bcaf9
Thibaut Lartigue <thibaut.lartigue.pro@gmail.com>
2022-08-22 11:07:36 +0200

    done iteration 2

--- src/clue.js
+++ src/clue.js
@@ -117,9 +117,18 @@ const weaponsArray = [
 
 // ITERATION 2
 
-function selectRandom() {}
+function selectRandom(arr) {
+    let randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex]
+}
 
-function pickMystery() {}
+function pickMystery() {
+    return {
+        suspect: selectRandom(suspectsArray),
+         weapon: selectRandom(weaponsArray),
+        room: selectRandom(roomsArray)
+      }
+}
 
 
 // ITERATION 3
